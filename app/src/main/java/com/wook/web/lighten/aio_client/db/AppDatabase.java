c9cfd5041c0b973b2cf55e10477c69163c47004d
jihoon4372 <60688328+jihoon4372@users.noreply.github.com>
2022-08-30 12:58:47 +0900

    add migration

--- app/src/main/java/com/wook/web/lighten/aio_client/db/AppDatabase.java
+++ app/src/main/java/com/wook/web/lighten/aio_client/db/AppDatabase.java
@@ -2,11 +2,14 @@ package com.wook.web.lighten.aio_client.db;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
-@Database(entities = {Report.class}, version = 1)
+@Database(entities = {Report.class}, version = 2)
 
 public abstract class AppDatabase extends RoomDatabase {
 
@@ -18,8 +21,16 @@ public abstract class AppDatabase extends RoomDatabase {
         if(INSTANCE == null){
             INSTANCE = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "Report")
                     .allowMainThreadQueries()
+                    .addMigrations(MIGRATIONFrom1To2)
                     .build();
         }
         return  INSTANCE;
     }
+
+    static final Migration MIGRATIONFrom1To2 = new Migration(1, 2) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("DELETE FROM Report");
+        }
+    };
 }
